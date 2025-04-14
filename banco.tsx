340638f5940f81e2c8b395e0ebd3775f33bc285f
Alexander Erbiste <alexander.erbiste@gmail.com>
2025-04-14 20:44:46 -0300

    atualização com o banco de daods criando tabela.

--- banco.tsx
+++ banco.tsx
@@ -1,12 +1,12 @@
 import react from "react";
 import { Button, View } from "react-native"
 import * as SQLite from 'expo-sqlite';
-
-
+ 
+ 
 // componentes
-
+ 
  const Banco = ()=>{
-
+ 
     async function criaDatabase(){
         const db = await SQLite.openDatabaseAsync('PAM2');
         if(db){
@@ -14,15 +14,51 @@ import * as SQLite from 'expo-sqlite';
         }else{
             console.log('erro');
         }
+        return db;
     }
-
+ 
+    async function createTABLE(){
+        try {
+            let db = await criaDatabase();
+            await db.execAsync(`
+                PRAGMA journal_mode = WAL;
+                CREATE TABLE IF NOT EXISTS tb_usuario
+                (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);`            
+            );
+            console.log('tabela criada')
+        }catch (error) {
+            console.error('Erro ao criar tabela:', error);
+        }
+    }
+ 
+    async function inserirDADOS(){
+        try {
+            let db = await criaDatabase();
+            await db.execAsync(`INSERT INTO tb_usuario (id, nome) VALUES (1, 'Zé Ruela');`);            
+            
+            console.log('DADOS INSERIDOS')
+        }catch (error) {
+            console.error('Erro ao criar tabela:', error);
+        }
+    }
+  
     return(
         <View>
             <Button
             title="Create Banco"
             onPress={criaDatabase}
             />
+           
+            <Button
+                title="Criar Tabela"
+                onPress={()=>{createTABLE()}}
+            />
+            <Button
+                title="Criar Tabela"
+                onPress={()=>{inserirDADOS()}}
+            />
         </View>
     )
  }
  export default Banco;
+ 
